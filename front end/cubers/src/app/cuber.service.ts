4f472cc8338d4822a5f78f09f6f737c2362c761e
fletcher-berry <32848772+fletcher-berry@users.noreply.github.com>
2019-01-25 09:51:33 -0600

    Front end: put server address in configuration

--- front end/cubers/src/app/cuber.service.ts
+++ front end/cubers/src/app/cuber.service.ts
@@ -1,31 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Cuber} from './models/cuber';
+import {CuberSummary} from './models/CuberSummary'
 import {Observable} from 'rxjs';
-import {CuberSummary} from './models/CuberSummary';
+import {environment} from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuberService {
 
-  private baseUrl = "https://localhost:44318";
-
   constructor(private http: HttpClient) { }
 
   public getCuberSummary(): Observable<CuberSummary>{
-    return this.http.get<CuberSummary>(this.baseUrl + "/cuber");
+    return this.http.get<CuberSummary>(environment.serverAddress + "/cuber");
   }
 
   public getCuber(id: number): Observable<Cuber>{
-      return this.http.get<Cuber>(this.baseUrl + "/cuber/" + id);
+      return this.http.get<Cuber>(environment.serverAddress + "/cuber/" + id);
   }
 
   public deleteCuber(id :number): Observable<{}>{
-    return this.http.delete(this.baseUrl + "/cuber/" + id);
+    return this.http.delete(environment.serverAddress + "/cuber/" + id);
   }
 
-  public updateCuber(cuber: Cuber): Observable<{}>{
-    return this.http.post(this.baseUrl + "/cuber/", cuber);
+  public updateCuber(cuber: Cuber): Observable<Cuber>{
+    return this.http.post<Cuber>(environment.serverAddress + "/cuber/", cuber);
   }
 }
